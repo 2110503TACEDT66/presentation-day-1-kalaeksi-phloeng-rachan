f3afa37837db39ecc1db73a1c91e98d12c25ae1c
webbalaka <webbalaka@gmail.com>
2024-03-02 02:18:31 +0700

    add MassageShop Schema

---
+++ model/MassageShop.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const MassageShopSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: [true, "Please add a name"],
+        trim: true,
+    },
+    address: {
+        type: String,
+        require: [true, "Please add a address"],
+    },
+    telephone: {
+        type: String,
+        require: [true, "Please add a telephone"],
+    },
+    open: {
+        type: Date,
+        require: [true, "Please add a open time"],
+    },
+    close: {
+        type: Date,
+        require: [true, "Please add a close time"],
+    }
+});
+
+module.exports = mongoose.model("MassageShopSchema", MassageShopSchema);
