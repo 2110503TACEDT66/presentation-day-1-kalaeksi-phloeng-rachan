b5ade124dcb69d946b2ba7f7d45ce987d48c553b
webbalaka <webbalaka@gmail.com>
2024-03-02 02:45:01 +0700

    Connect MongoDB

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db.js");
+
+dotenv.config({path: "./config/config.env"});
+
+connectDB();
 
 const app = express();
+
+app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(
