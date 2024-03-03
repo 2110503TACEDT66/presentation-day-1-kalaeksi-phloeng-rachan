32c4ecf3df8eef6644af233b57e17bca65bfed80
LapisBerry <141068821+LapisBerry@users.noreply.github.com>
2024-03-04 04:06:19 +0700

    recheck+comment middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,20 +1,25 @@
+/**
+ * @LapisBerry
+ * 2024 MAR 3 04:05:00 AM
+ * All Clear
+ */
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
-//protect routes
+// Protect routes
 exports.protect = async (req,res,next) =>{
     let token;
     if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
         token = req.headers.authorization.split(' ')[1];
     }
 
-    //make sure token exists
+    // Make sure token exists
     if(!token || token == 'null'){
         return res.status(401).json({success:false, message: 'Not authorize to access this route'});
     }
 
     try{
-        //verify token
+        // Verify token
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         console.log(decoded);
         req.user = await User.findById(decoded.id);
@@ -26,11 +31,14 @@ exports.protect = async (req,res,next) =>{
     }
 }
 
-//grant access to specific roles
+// Grant access to specific roles
 exports.authorize = (...roles) => {
     return (req,res,next) => {
         if(!roles.includes(req.user.role)){
-            return res.status(403).json({success: false, message : `User role ${req.user.role} is not authorized to access this route`});
+            return res.status(403).json({
+                success: false,
+                message : `User role ${req.user.role} is not authorized to access this route`
+            });
         }
         next();
     }
