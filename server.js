b7bd23fc4703dad00229bd73953633e5887cd408
webbalaka <webbalaka@gmail.com>
2024-03-26 15:43:17 +0700

    fix server.js

--- server.js
+++ server.js
@@ -6,16 +6,18 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db.js");
-const cookieParser = require('cookie-parser');
-const mongoSanitize = require('express-mongo-sanitize');
-const {xss} = require('express-xss-sanitizer');
-const helmet = require('helmet');
-const rateLimit = require('express-rate-limit');
-const hpp = require('hpp');
-const cors = require('cors');
+const cookieParser = require("cookie-parser");
+const mongoSanitize = require("express-mongo-sanitize");
+const { xss } = require("express-xss-sanitizer");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require("cors");
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express");
 
 // Load env vars
-dotenv.config({path: "./config/config.env"});
+dotenv.config({ path: "./config/config.env" });
 
 // Connect to database
 connectDB();
@@ -30,7 +32,8 @@ const MassageShop = require("./routes/massageShop");
 const reservations = require("./routes/reservations");
 const Auth = require("./routes/auth");
 const Review = require("./routes/reviews");
-const Otp = require("./routes/otp")
+const Otp = require("./routes/otp");
+const swaggerJSDoc = require("swagger-jsdoc");
 
 // Cookie parser
 app.use(cookieParser());
@@ -46,8 +49,8 @@ app.use(xss());
 
 // Rate limiting
 const limiter = rateLimit({
-    windowsMs : 10*60*1000, //10 mins
-    max : 500
+	windowsMs: 10 * 60 * 1000, //10 mins
+	max: 500,
 });
 app.use(limiter);
 
@@ -59,14 +62,41 @@ app.use(cors());
 
 // Mount routers
 app.use("/api/massageShops", MassageShop);
-app.use("/api/reservations", reservations)
+app.use("/api/reservations", reservations);
 app.use("/api/auth", Auth);
 app.use("/api/review", Review);
 app.use("/api/otp", Otp);
 
+const swaggerOptions = {
+	swaggerDefinition: {
+		openapi: "3.0.0",
+		info: {
+			title: "My API",
+			version: "1.0.0",
+			description: "A simple Express Library API",
+		},
+        servers: [
+            {
+                url: `${process.env.HOST}:${process.env.PORT}/api`
+            }
+        ]
+	},
+	apis: ["./routes/*.js"], // Specify the paths to your API route files
+};
+
+const swaggerDocs=swaggerJSDoc(swaggerOptions);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 const PORT = process.env.PORT || 5000;
-const server = app.listen(PORT, console.log("Server running in ", process.env.NODE_ENV, " mode on port ", PORT));
+const server = app.listen(
+	PORT,
+	console.log(
+		"Server running in ",
+		process.env.NODE_ENV,
+		" mode on port ",
+		PORT
+	)
+);
 
 process.on("unhandledRejection", (err, promise) => {
 	console.log(err.message);
